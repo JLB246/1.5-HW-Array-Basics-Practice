03e19c8cc8e20f5b78357ed35cdf6abbed508a7f
JLB246 <65634399+JLB246@users.noreply.github.com>
2020-07-29 15:34:14 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+/*
+ Challenge 1: Create an array containing the last
+ names of the first 5 US presidents in order, and 
+ assign it to a variable called "presidents". 
+ 
+
+ Make sure your spelling is correct, or the automated
+ tests will not run correctly.
+*/
+
+/*
+ Challenge 2: Create an array of the first 5
+ US states in alphabetical order, and assign it
+ to a variable called "states".
+ 
+ Using bracket syntax, add the sixth state by 
+ alphabetical order ('Colorado') to the end of this
+ array.
+*/
+
+/*
+ Challenge 3: Log the length of each of your two 
+ arrays, in two separate statements.
+*/
+
+
+
+ var presidents = ['Washington','Adams','Jefferson','Madison','Monroe'];
+ 
+ console.log(presidents.length)
+
+ 
+ var states = ['Connecticut','Delaware','Georgia','New Jersey','Pennsylvania'];
+ states [5] = 'Colorado'
+ 
+ console.log(states.length)
